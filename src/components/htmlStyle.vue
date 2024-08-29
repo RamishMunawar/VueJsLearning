b94694958fbcfab62b1d22c37e3924bc71fb3a8b
Ramish Munawar <128965516+RamishMunawar@users.noreply.github.com>
2024-08-29 13:24:05 +0500

    components\htmlStyle.vue

--- src/components/htmlStyle.vue
+++ src/components/htmlStyle.vue
@@ -7,12 +7,39 @@
      style with property
 -->
 <template>
-<div>
-    <h1 class="">class and style binding</h1>
-</div>
-</template>
-<script>
-export default {
-    name:'htmlStyle'
-}
-</script>
+    <div>
+        <h1 class="textStyle">class and style binding</h1>
+        <h1 v-bind:style="{color:'blue'}">class and style binding</h1>
+        <h1 v-bind:class="{display:show,displayColor:show}">class binding with object</h1>
+        <h1 v-bind:class="[aclass]">class binding with array</h1>
+    </div>
+    </template>
+    <script>
+    export default {
+        name:'htmlStyle',
+        data(){
+            return{
+                show:true,
+                aclass:'arrayClass'
+                
+            }
+        }
+    }
+    </script>
+    
+    
+    <style>
+    .textStyle{
+        color:orange;
+        
+    }
+    .display{
+        background-color: red;
+    }
+    .displayColor:hover{
+        color:orange;
+    }
+    .arrayClass{
+        background-color: cornflowerblue;
+    }
+    </style>
