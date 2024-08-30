9986ca9d38e2e64fa49adb4b0ae74e1235e41322
Ramish Munawar <128965516+RamishMunawar@users.noreply.github.com>
2024-08-30 16:14:53 +0500

    components\signup>form vue input checkbox and radio

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,42 @@
   <div id="app">
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld />
-  <h1>{{ title }}</h1>
+  <h1>From with checkbox and radio button in vue js</h1>
+  <!-- <h1>{{ title }}</h1> -->
   <!-- <Homepage data="Ramish Munawar" msg="new"/>  -->
   <!-- <childComponent /> -->
    <Templates />
    <!-- <htmlStyle /> -->
     <!-- <DataBinding /> -->
      <!-- <Props v-bind:users='users'/> -->
-      <childToParentProps v-on:changetitle="updateTitle($event)"/>
+      <!-- <childToParentProps v-on:changetitle="updateTitle($event)"/> -->
+       <signUp />
 </div>
 </template>
 
 <script>
 // import childComponent from './components/childComponent.vue'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 // import Homepage from './components/Homepage.vue'
 // import Templates from './components/Templates.vue'
 // import htmlStyle  from './components/htmlStyle.vue'
 // import DataBinding from './components/DataBinding.vue'
 // import Props from './components/Props.vue'
-import childToParentProps from './components/childToParentProps.vue'
+// import childToParentProps from './components/childToParentProps.vue'
+import signUp  from './components/signUp.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
     // Homepage,
     // childComponent,
     // Templates,
     // htmlStyle
     // DataBinding
     // Props
-    childToParentProps,
+    // childToParentProps,
+    signUp
+
     
    
   },
---
+++ src/components/signUp.vue
@@ -0,0 +1,48 @@
+<!-- 
+    How to make from in vue js
+    Make a component and import in app.vue file
+    Make input fields
+    Add checkbox and radio button
+    Define property for form
+    Bind input field with property
+    submit form
+    -->
+    <template>
+        <div>
+            <input type="text" name="" id="" placeholder="enter your name" v-model="signup.username"><br><br>
+            <input type="password" name="" id="" placeholder="enter your password" v-model="signup.password"><br><br>
+            <p>Chooese your Hobbies</p>
+            <input type="checkbox"  id="cricket" value="cricket" v-model="signup.hobbies">
+            <label for="cricket">Cricket</label>
+            <input type="checkbox"  id="football" value="football" v-model="signup.hobbies">
+            <label for="football">FootBall</label><br>
+            <p>Chooese your gender</p>
+            <input type="radio"  id="male" value="male" v-model="signup.gender">
+            <label for="male">Male</label>
+            <input type="radio"  id="female" value="female" v-model="signup.gender">
+            <label for="female">Female</label><br><br>
+            <button v-on:click="submitData()">Submit</button>
+        </div>
+    </template>
+    <script>
+     export default {
+        name:'signUp',
+        data(){
+            return{
+                signup:{
+                username:null,
+                password:null,
+                hobbies:[],
+                gender:null
+            }
+            }
+
+          
+        },
+        methods:{
+            submitData(){
+                console.log(this.signup)
+            }
+        }
+     }
+</script>
